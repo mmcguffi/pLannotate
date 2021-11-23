c085b9109cb92c73a9fc9643bbadc5a2f6bbbaf4
Matt McGuffie <mmcguffi@gmail.com>
2021-11-23 15:56:34 -0600

    moved details into annotate script

--- plannotate/annotate.py
+++ plannotate/annotate.py
@@ -10,7 +10,6 @@ import pandas as pd
 import streamlit as st
 
 import plannotate.resources as rsc
-from plannotate.BLAST_hit_details import details
 from plannotate.infernal import parse_infernal
 
 log = NamedTemporaryFile()
@@ -186,7 +185,83 @@ def clean(inDf):
 
     return inDf
 
-@st.cache(hash_funcs={pd.DataFrame: lambda _: None}, suppress_st_warning=True, max_entries = 10, show_spinner=False)
+
+def get_details(inDf, yaml_file_loc):
+    
+    def parse_gz(sseqids, gz_loc):
+    #this is a bit fragile right now -- requires ['sseqid','Feature','Description'] order
+    #as well as a default type
+    #currently this is only implemented for the large SwissProt db
+    #Could scrape first line to infer what is given that way
+        hits = "|".join(sseqids)
+        output = NamedTemporaryFile(suffix="csv")
+        subprocess.call(f'rg -z "{hits}" {gz_loc} > {output.name}',shell = True)
+        gz_details = pd.read_csv(output.name, header = None, names=['sseqid','Feature','Description'])
+        output.close()
+        return gz_details
+    
+    #loop through databases
+    databases = rsc.get_yaml(yaml_file_loc)
+    
+    assert len(set(inDf['db'].to_list())) == 1, "All hits must be from the same database"
+    database_name = inDf['db'].to_list()[0]
+    
+    
+    database = databases[database_name]
+    
+    sseqids = inDf.loc[inDf['db'] == database_name]['sseqid'].tolist()
+    sseqids = [_ for _ in sseqids if _] #removes blank edgecases
+
+    db_details = database['details']
+
+    if db_details['location'] == 'None':
+        #if no file is passed, data should already be in dataframe
+        feat_desc = inDf.loc[inDf['db'] == database_name][['sseqid','Feature','Description']]
+        
+    else:
+        if db_details['location'] == "Default":
+            details_file_loc = rsc.get_details(database_name) + ".csv"
+        else: #if a file path is passed, use that
+            details_file_loc = db_details['location']
+
+        #if the description file is compressed
+        if db_details['compressed'] == True:
+            details_file_loc +=  ".gz"
+            feat_desc = parse_gz(sseqids, details_file_loc) 
+        else: #if it is uncompressed
+            feat_desc = pd.read_csv(details_file_loc)
+        
+        # bespoke extraction of swissprot protein exisitence level
+        if database_name == 'swissprot':
+            level = feat_desc['Description'].str.find("existence level") + 16 #len of "existence level" + 1
+            feat_desc['s'] = level
+            feat_desc['e'] = level + 1
+
+            def calc_priority_mod(d,s,e):
+            # if 'existence level' is not found, 
+            # 0 is returned as the location
+            # meaning 15 and 16 are the default values
+            # this sets a baseline priority of `1` if nothing is found
+                if s == 15 and e == 16:
+                    return 0
+                else:
+                    return int(d[s:e]) - 1
+
+            # extract the level from the description
+            feat_desc['priority_mod'] = [calc_priority_mod(d,s,e) for d, s, e in zip(feat_desc["Description"], feat_desc["s"], feat_desc["e"])]
+            feat_desc = feat_desc.drop(columns=['s','e'])
+
+    #try to see if a default type was passed
+    if db_details['default_type'] != 'None':
+        feat_desc['Type'] = db_details['default_type']
+    else:
+        pass 
+    
+    return feat_desc
+
+
+
+#@st.cache(hash_funcs={pd.DataFrame: lambda _: None}, suppress_st_warning=True, max_entries = 10, show_spinner=False)
 def get_raw_hits(query, linear, yaml_file_loc):
      
     progressBar = st.progress(0)
@@ -207,7 +282,12 @@ def get_raw_hits(query, linear, yaml_file_loc):
         if hits.empty:
             continue
         
-        hits = details(hits, yaml_file_loc)
+        feat_descriptions = get_details(hits, yaml_file_loc)
+        # `suffixes = ('_x', None)` means the descriptions for Rfam will be copied,
+        # the original descriptions will be appeneded with `_x` and can be ignored
+        # the Rfam descriptions are in the original df due to the quirks of how the details
+        # are stored, so this is a work around. Possibly condsider dropping the `_x`` column
+        hits = hits.merge(feat_descriptions, on='sseqid', how='left', suffixes = ('_x', None))
         
         #removes primer binding site annotations
         hits = hits.loc[hits['Type'] != 'primer bind']
@@ -260,18 +340,7 @@ def annotate(inSeq, yaml_file, linear = False, is_detailed = False):
     
     #this has to re-parse the yaml, so not an elegant solution
     if is_detailed == True:
-        databases = rsc.get_yaml(yaml_file)
-        dbs_used = set(blastDf['db'].to_list())
-        for database_name in dbs_used:
-            database = databases[database_name]
-            detail = database['details']
-            try:
-                blastDf.loc[blastDf['db'] == database_name, 'kind'] = detail['default_type']
-            except KeyError:
-                if detail['file'] == True:
-                    blastDf.loc[blastDf['db'] == database_name, 'kind'] = blastDf.loc[blastDf['db'] == database_name, 'Type']
-                else:
-                    st.error("details error")
+        blastDf['kind'] = blastDf['Type']
     else:
         blastDf['kind'] = 1
     
