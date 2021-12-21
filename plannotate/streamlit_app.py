7114722c3e28039de56aef7ee2c543f7a0918c6e
Matt McGuffie <mmcguffi@gmail.com>
2021-12-21 13:26:49 -0600

    added blurb about detailed annotation

--- plannotate/streamlit_app.py
+++ plannotate/streamlit_app.py
@@ -88,6 +88,8 @@ def run_streamlit(args): #args
                 st.bokeh_chart(get_bokeh(recordDf, linear), use_container_width=False)
                 if linear:
                     st.write("\*plasmid is displayed as cirucular, though pLannotate is treating this as a linear construct")
+                if detailed:
+                    st.write("\*\*pLannotate is running in Detailed Annotation mode which can find more hits, though may also find more false positives.")
 
                 st.header("Download Annotations:")
 
