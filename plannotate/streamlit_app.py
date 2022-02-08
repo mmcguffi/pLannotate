e524a210c69250774ad0de2747620b3d9b853129
Matt McGuffie <mmcguffi@gmail.com>
2022-02-08 10:48:51 -0600

    spelling fix

--- plannotate/streamlit_app.py
+++ plannotate/streamlit_app.py
@@ -87,7 +87,7 @@ def run_streamlit(args): #args
                 st.write("Hover mouse for info, click and drag to pan, scroll wheel to zoom")
                 st.bokeh_chart(get_bokeh(recordDf, linear), use_container_width=False)
                 if linear:
-                    st.write("\*plasmid is displayed as cirucular, though pLannotate is treating this as a linear construct")
+                    st.write("\*plasmid is displayed as circular, though pLannotate is treating this as a linear construct")
                 if detailed:
                     st.write("\*\*pLannotate is running in Detailed Annotation mode which can find more hits, though may also find more false positives.")
 
