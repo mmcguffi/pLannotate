0d8662f7e3cf4d82202f2e75859f2f50a0208b69
Matt McGuffie <mmcguffi@gmail.com>
2022-04-11 19:38:21 -0500

    changed to tox.ini so flake8/black works properly

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,21 +1,31 @@
 from plannotate import resources
 from plannotate import annotate
-
+import pytest
 
 RRNB = "tgaccaggcatcaaataaaacgaaaggctcagtcgaaagactgggcctttcgttttatctgttgtttgtcggtgaacgctctctact"
 
+
 def test_annotate():
     hits = annotate.annotate(RRNB)
-    assert hits.iloc[0]['sseqid'] == "rrnB_T1_terminator"
+    assert hits.iloc[0]["sseqid"] == "rrnB_T1_terminator"
+
 
 def test_yaml():
     db_meta = resources.get_yaml(resources.get_yaml_path())
-    snapgene_db = db_meta['snapgene']
+    snapgene_db = db_meta["snapgene"]
     assert isinstance(snapgene_db, dict)
+
 
 def test_BLAST():
     db_meta = resources.get_yaml(resources.get_yaml_path())
-    snapgene_db = db_meta['snapgene']
+    snapgene_db = db_meta["snapgene"]
     hits = annotate.BLAST(RRNB, snapgene_db)
     assert not hits.empty
-    
+
+
+def test_nothing():
+    assert True
+
+
+def test_get_fasta():
+    print(resources.get_example_fastas())
