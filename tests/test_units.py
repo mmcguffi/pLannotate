77c5e70b0d3c3303bc2b57201538451c1aa663db
Matt McGuffie <mmcguffi@gmail.com>
2022-06-17 18:21:36 -0500

    add tests

--- tests/test_units.py
+++ tests/test_units.py
@@ -18,14 +18,26 @@ def test_BLAST():
     hits = annotate.BLAST(RRNB, snapgene_db)
     assert not hits.empty
 
-
-def test_nothing():
-    assert True
-
-
 def test_get_fasta():
     print(resources.get_example_fastas())
 
+def test_databases_exist():
+    assert resources.databases_exist() == True
+    
+def test_valid_sequence_correct():
+    resources.validate_sequence("ACTG")
+    
+def test_valid_sequence_incorrect_base():
+    try:
+        resources.validate_sequence("ACTX")
+        assert False
+    except ValueError:
+        pass
+
+def test_get_name_ext():
+    name, ext = resources.get_name_ext("./a/long/path/test.fasta")
+    assert name == "test"
+    assert ext == ".fasta"
 
 def test_streamlit_app():
     """this component is hard to test"""
@@ -35,3 +47,5 @@ def test_streamlit_app():
 def test_annotate():
     hits = annotate.annotate(RRNB)
     assert hits.iloc[0]["sseqid"] == "rrnB_T1_terminator"
+    
+    
