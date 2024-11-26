dfebf0838f53967e6fb94dbccfc56de02414b980
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-11-26 18:33:38 +0000

    minor fixes + max length accepted via env vars

--- plannotate/api.py
+++ plannotate/api.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Query
 from fastapi.responses import JSONResponse, RedirectResponse
+from typing import Literal
 import io
 from pydantic import BaseModel
 from plannotate.annotate import annotate
 from Bio.SeqIO import read
 from plannotate import resources as rsc
+from plannotate import __version__ as version
+import os
+
+MAX_SEQUENCE_LENGTH = (
+    os.environ["MAX_SEQUENCE_LENGTH"] if "MAX_SEQUENCE_LENGTH" in os.environ else 20000
+)
 
 
 class ReportRow(BaseModel):
@@ -27,6 +34,7 @@ class ReportRow(BaseModel):
 class AnnotateResponse(BaseModel):
     gb_file: str
     report: list[ReportRow]
+    version: Literal[f"{version}"]
 
 
 app = FastAPI()
@@ -38,7 +46,9 @@ async def root():
 
 
 @app.post("/annotate", response_model=AnnotateResponse)
-async def analyze_sequence(file: UploadFile = File(...)):
+async def analyze_sequence(
+    file: UploadFile = File(...), circular: bool = Query(default=False)
+):
     # Read uploaded file contents
     contents = await file.read()
     try:
@@ -59,13 +69,28 @@ async def analyze_sequence(file: UploadFile = File(...)):
     except Exception:
         raise HTTPException(status_code=400, detail="Error reading file")
 
+    circular = circular or (
+        "topology" in record.annotations.keys()
+        and record.annotations["topology"] == "circular"
+    )
     inSeq = str(record.seq)
-    recordDf = annotate(inSeq, is_detailed=True, linear=False)
-    gbk = rsc.get_gbk(recordDf, inSeq, is_linear=False)
+    if len(inSeq) > MAX_SEQUENCE_LENGTH:
+        raise HTTPException(
+            400,
+            f"Sequence length is greater than the maximum allowed length of {MAX_SEQUENCE_LENGTH} characters",
+        )
+    recordDf = annotate(inSeq, is_detailed=True, linear=not circular)
+    gbk = rsc.get_gbk(recordDf, inSeq, is_linear=not circular)
     csv = rsc.get_clean_csv_df(recordDf)
+
+    fixed_report = [
+        {k.replace(" ", "_"): v for k, v in r.items()}
+        for r in csv.to_dict(orient="records")
+    ]
     response = {
+        "version": version,
         "gb_file": gbk,
-        "report": csv.to_dict(orient="records"),
+        "report": fixed_report,
     }
 
     return JSONResponse(content=response)
