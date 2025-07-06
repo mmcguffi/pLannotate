1be2fcceef67e87859193e77d502a11bb9de9668
Matt McGuffie <mmcguffi@gmail.com>
2025-07-06 16:57:55 -0700

    + module level logging

--- plannotate/__init__.py
+++ plannotate/__init__.py
@@ -1,5 +1,10 @@
 __version__ = "1.2.2"
+import logging
 
+from .logging_config import setup_logging
 from .models import Construct
+
+# Set up logging once at module level
+setup_logging(level=logging.INFO)
 
 __all__ = ["Construct"]
