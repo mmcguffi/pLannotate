bb0c9dc128106220ba6d01549999c6950e11a3ac
Daniel Standage <daniel.standage@st.dhs.gov>
2026-02-23 14:21:05 -0500

    Troubleshoot CI

--- plannotate/resources.py
+++ plannotate/resources.py
@@ -338,10 +338,9 @@ def download_databases():
     # this is locked at minor version bumps
     # need to upload a new database into github every minor update
     # patch number bumps just refer to the X.X.0 version
-    version_parts = plannotate_version.split('.', 1)
+    version_parts = plannotate_version.split('.')
     version = ".".join(version_parts[:2])
     db_loc = f"https://github.com/mmcguffi/pLannotate/releases/download/v{version}.0/BLAST_dbs.tar.gz"
-    # db_loc = "https://github.com/barricklab/pLannotate/releases/download/v1.1.0/BLAST_dbs.tar.gz"
 
     # subprocess.call(["wget", "-P", f"{ROOT_DIR}/data/", db_loc])
     subprocess.run(["curl", "-L", "-o", f"{ROOT_DIR}/data/BLAST_dbs.tar.gz", db_loc], check=True)
